af68f41c44f57d616dc374e0131af5adec60e6bf
ryandavidmercado <ryandavidmercado@gmail.com>
2021-05-10 15:34:51 -0400

    Remove additional instructional comment.

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,6 @@ const controller = require("./dishes.controller");
 const router = require("express").Router();
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
 router
   .route("/")
   .get(controller.list)
